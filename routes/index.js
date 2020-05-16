b55b0c95cd5d78666d2dfb688b7bb6df554676c4
Ryou_Changyu <ycg02116@kyonggi.ac.kr>
2020-05-16 16:50:16 +0900

    기능추가: FCM 푸시
    FCM 푸시

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var db = require('./db');
+var fcm = require('./sendFCM');
 
 /* GET home page. */
 router.get('/', function(req, res) {
+  res.set({'access-control-allow-origin': '*'});
   res.status(200).json(
     {
       "success" : true
@@ -11,9 +13,10 @@ router.get('/', function(req, res) {
   );
 });
 
-router.get('/dust_1', db.dust_1); //localhost:3030/dust_1에 db.dust_1 함수를 매핑
+router.get('/dust_1', db.dust_1);               //localhost:3030/dust_1에 db.dust_1 함수를 매핑
 router.get('/dust_25',db.dust_25);
 router.get('/dust_10',db.dust_10);
 router.post('/dbins', db.dbinsert);
+router.get('/fcm', fcm.sendNoti);               //send FCM Message
  
 module.exports = router;
