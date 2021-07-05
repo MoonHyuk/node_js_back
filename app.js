d1d73f6dd1d976bf847b1a207f7cca08fcb8a8ac
moonhyuk <mo_onhyuk@naver.com>
2021-07-05 23:43:43 +0900

    public path 재설성

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(cookieParser());
 
 app.use(cleanEvery2Min.cleanInterval);
 app.use(insertLongTerm.insertInterval);
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/", indexRouter);
-
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
 	next(createError(404));
