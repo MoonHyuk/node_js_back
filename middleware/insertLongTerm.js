03fe9d90cbf754b269adfa033b4efbc6bf25964c
moonhyuk <mo_onhyuk@naver.com>
2021-12-29 16:12:52 +0900

    버그 수정

--- middleware/insertLongTerm.js
+++ middleware/insertLongTerm.js
@@ -10,29 +10,31 @@ async function insertInterval(req, res, next) {
 	if (flag) {
 		setInterval(() => {
 			sensors.forEach(sensor => {
-				liveData(sensor).then(res => {
-					if (res.result[0].length === 0) return;
+				for (let i = 1; i <= 4; i ++) {
+					liveData(sensor, i).then(res => {
+						if (res.result[0].length === 0) return;
 
-					const recentValue = res.result[0][0];
-					const now = moment().tz("Asia/Seoul");
-					const minuteDiff = moment.tz(recentValue['checkTime'], "Asia/Seoul").diff(now, 'minutes');
+						const recentValue = res.result[0][0];
+						const now = moment().tz("Asia/Seoul");
+						const minuteDiff = moment.tz(recentValue['checkTime'], "Asia/Seoul").diff(now, 'minutes');
 
-					if (Math.abs(minuteDiff) > 1) return;
+						if (Math.abs(minuteDiff) > 1) return;
 
-					let type = sensor.slice(0, sensor.length - 4);
-					if (type === "pm25") {
-						type = "pm2.5";
-					}
+						let type = sensor.slice(0, sensor.length - 4);
+						if (type === "pm25") {
+							type = "pm2.5";
+						}
 
-					const sql = "INSERT INTO long_term (type, checkTime, ppm, sensorId, factoryId) VALUES (?, ?, ?, ?, ?);";
-					pool.query(sql, [
-						type,
-						recentValue['checkTime'],
-						recentValue['ppm'],
-						recentValue['sensorId'],
-						recentValue['factoryId'],
-					]);
-				});
+						const sql = "INSERT INTO long_term (type, checkTime, ppm, sensorId, factoryId) VALUES (?, ?, ?, ?, ?);";
+						pool.query(sql, [
+							type,
+							recentValue['checkTime'],
+							recentValue['ppm'],
+							recentValue['sensorId'],
+							recentValue['factoryId'],
+						]);
+					});
+				}
 			})
 		}, 30000);
 		flag = false;
